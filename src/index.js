c7300f62ee9f3376c2c750dc2b7444f941107994
Pierce Issah <PJIceskull2003@gmail.com>
2023-12-05 09:53:36 -0500

    install firebase to project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 // import functions from other files
 import { changeRoute } from "./model.js";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import {
+  getAuth,
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth"; // look for the auth folder
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAauj-zBAp9ewpyD6J7IGH5zjJPaQjRWSI",
+  authDomain: "n315-pi.firebaseapp.com",
+  projectId: "n315-pi",
+  storageBucket: "n315-pi.appspot.com",
+  messagingSenderId: "301611833853",
+  appId: "1:301611833853:web:2fc92b7326a5fab7d9e194",
+  measurementId: "G-FWVH83GF36",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
 // Global Variables
 
 //
